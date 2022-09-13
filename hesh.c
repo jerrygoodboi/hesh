7cb228f50667b70f076cc2603206359fe357ca40
Jerry <rabbitron295@gmail.com>
2022-09-13 16:56:07 +0530

    NOthing

--- hesh.c
+++ hesh.c
@@ -2,16 +2,19 @@
 int main(){
 char string[20];
 int i=0;
-printf("Enter the string: ");
-gets(string);
-while(string[i]!='\0'){
-if(string[i]>31&&string[i]<80){
-string[i]=string[i]+47;
-}
-else if(string[i]>79&&string[i]<127){
-string[i]=string[i]-47;
-}
-i++;}
+
+
+ printf("Please don't type more than 20 characters, if you want so edit the source code.\n");
+ printf("Enter the string: ");
+   gets(string);
+   while(string[i]!='\0'){
+        if(string[i]>31&&string[i]<80){
+                      string[i]=string[i]+47;
+                                      }
+        else if(string[i]>79&&string[i]<127){
+                      string[i]=string[i]-47;
+                                            }
+   i++;}
 puts(string);
 }
 
