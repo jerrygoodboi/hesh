dad3136a12d5ad1de07b46194218f4eb47ec4507
Jerry Ron Sunny <rabbitron295@gmail.com>
2022-09-16 19:14:45 +0530

    dehesh solved

--- dehesh.c
+++ dehesh.c
@@ -5,7 +5,7 @@ int i=0;
  printf("Enter the string: ");
  gets(string);
  while(string[i]!='\0'){
- if(string[i]>79&&string[i]<127){
+ if(string[i]>=79&&string[i]<127){
  string[i]=string[i]-47;
  }
  else if(string[i]>31&&string[i]<80){
