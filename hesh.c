47899b14edd6ac96b7a46d9c61c6681d869c5036
jerrygoodboi <87520422+jerrygoodboi@users.noreply.github.com>
2023-01-26 20:00:31 +0530

    Update hesh.c

--- hesh.c
+++ hesh.c
@@ -4,10 +4,10 @@ int main(int argc,char **argv){
 char *string=argv[1];
 int i=0;
    while(string[i]!='\0'){
-        if(string[i]>31&&string[i]<80){
+        if(string[i]>31&&string[i]<79){
                       string[i]=string[i]+47;
                                       }
-        else if(string[i]>79&&string[i]<127){
+        else if(string[i]>78&&string[i]<127){
                       string[i]=string[i]-47;
                                             }
    i++;}
